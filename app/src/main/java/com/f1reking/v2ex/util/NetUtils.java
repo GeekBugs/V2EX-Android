e59577e2b330ddb8b911bf740eeb1a94916bf038
F1ReKing <284061805@qq.com>
2016-01-28 21:06:40 +0800

    update

--- app/src/main/java/com/f1reking/v2ex/util/NetUtils.java
+++ app/src/main/java/com/f1reking/v2ex/util/NetUtils.java
@@ -14,6 +14,12 @@ import android.telephony.TelephonyManager;
  */
 public class NetUtils {
 
+    public static final int NETWORK_CLASS_UNKOWN = 0;
+    public static final int NETWORK_WIFI = 1; //wifi
+    public static final int NETWORK_CLASS_2_G = 2; //2G
+    public static final int NETWORK_CLASS_3_G = 3; //3G
+    public static final int NETWORK_CLASS_4_G = 4; //4G
+
     private NetUtils() {
         /* cannot be instantiated */
         throw new UnsupportedOperationException("cannot be instantiated");
@@ -79,4 +85,61 @@ public class NetUtils {
         intent.setAction("android.intent.action.VIEW");
         activity.startActivityForResult(intent, 0);
     }
+
+    /**
+     * 判断手机链接的网络类型
+     * @param context
+     * @return
+     */
+    public static int getNetWorkCLass(Context context){
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        switch (telephonyManager.getNetworkType()){
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return NETWORK_CLASS_2_G;
+
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return NETWORK_CLASS_3_G;
+
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return NETWORK_CLASS_4_G;
+
+            default:
+                return NETWORK_CLASS_UNKOWN;
+        }
+    }
+
+    /**
+     * 判断当前手机的网络类型（wifi或者2G,3G,4G）
+     * @param context
+     * @return
+     */
+    public static int getNetWorkStatus(Context context){
+        int netWorkType = NETWORK_CLASS_UNKOWN;
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo!=null && networkInfo.isConnected()){
+            int type = networkInfo.getType();
+
+            if (type == ConnectivityManager.TYPE_WIFI){
+                netWorkType = NETWORK_WIFI;
+            }else if (type == ConnectivityManager.TYPE_MOBILE){
+                netWorkType = getNetWorkCLass(context);
+            }
+        }
+        return netWorkType;
+    }
+
+
 }
